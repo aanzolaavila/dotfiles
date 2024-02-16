f96840df753da52469a73cc1c6a4b004ab9a7b04
Alejandro Anzola <me@alejandroanzola.info>
2024-02-15 20:31:49 -0500

    small refactor

--- home/.chezmoiscripts/run_onchange_after_go.sh
+++ home/.chezmoiscripts/run_onchange_after_go.sh
@@ -4,19 +4,19 @@ set -eufo pipefail
 
 if [[ -z "${GOPATH}" ]]; then
   echo "GOPATH is not set"
-  exit
+  exit 1
 fi
 
 function go_install() {
-  local name=$1
-  local pkg=$2
+  local name="${1}"
+  local pkg="${2}"
 
   if [ -f "${GOPATH}/bin/${name}" ]; then
     return
   fi
 
   echo "Installing ${name}"
-  go install -v ${pkg}@latest
+  go install -v "${pkg}"@latest
 }
 
 go_install "gofumpt" "mvdan.cc/gofumpt"
