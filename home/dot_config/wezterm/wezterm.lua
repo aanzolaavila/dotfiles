afe37151329c063d83d79283f52a47688552d1cc
Alejandro Anzola <me@alejandroanzola.info>
2024-02-19 16:41:49 -0500

    adds better config for window opacity in wezterm

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -24,6 +24,18 @@ config.max_fps = 120
 config.animation_fps = 1 -- disable animations
 
 config.window_background_image = os.getenv('HOME') .. '/.config/wezterm/background'
-config.window_background_opacity = 0.3
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 40
+config.window_background_image_hsb = {
+	-- Darken the background image by reducing it to 1/3rd
+	brightness = 0.1,
+
+	-- You can adjust the hue by scaling its value.
+	-- a multiplier of 1.0 leaves the value unchanged.
+	hue = 1.0,
+
+	-- You can adjust the saturation also.
+	saturation = 0.6,
+}
 
 return config
