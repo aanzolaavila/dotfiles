35d6c6dc29efb642db845216813901c5559fd5b5
Alejandro Anzola <me@alejandroanzola.info>
2024-02-18 20:00:52 -0500

    changes wezterm max fps to 120

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -12,6 +12,6 @@ config.font = wezterm.font('Inconsolata Nerd Font Mono',
 config.enable_tab_bar = false
 config.cursor_blink_rate = 0 -- disable
 
-config.max_fps = 60
+config.max_fps = 120
 
 return config
