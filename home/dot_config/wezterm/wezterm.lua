7e545f63be86e3d1bc6aab4c031db125a418be12
Alejandro Anzola <me@alejandroanzola.info>
2024-02-19 15:49:29 -0500

    changes background name to be extension agnostic

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -23,7 +23,7 @@ end)
 config.max_fps = 120
 config.animation_fps = 1 -- disable animations
 
-config.window_background_image = os.getenv('HOME') .. '/.config/wezterm/background.jpg'
+config.window_background_image = os.getenv('HOME') .. '/.config/wezterm/background'
 config.window_background_opacity = 0.3
 
 return config
