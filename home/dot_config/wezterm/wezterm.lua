04e07a4932029f3b9393aa692249f6cbc14f2470
Alejandro Anzola <me@alejandroanzola.info>
2024-02-27 01:33:15 -0500

    removes font definition from Wezterm, works better with default one

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,14 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'Batman'
-config.font = wezterm.font('Inconsolata Nerd Font Mono',
-	{
-		stretch = 'Expanded',
-		weight = 'Regular',
-	}
-)
-
 config.enable_tab_bar = false
 config.cursor_blink_rate = 0 -- disable
 
